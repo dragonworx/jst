847787f030f165a65e4fd1a790f33bc35491bd63
Ali Chamas <achamas@atlassian.com>
2018-08-09 08:22:59 +1000

    refector

--- astQuery/index.js
+++ astQuery/index.js
@@ -1,2 +1,89 @@
-const astQuery = require('./astQuery');
-module.exports = astQuery;
+const path = require('path');
+const fs = require('fs');
+const chalk = require('chalk');
+const SourceFile = require('./sourceFile');
+const walk = require('./walk');
+
+const excludePath = './exclude.txt';
+const bypassExcludePath = './exclude.pause.txt';
+
+const print = val => console.log(JSON.stringify(val, null, 4));
+
+module.exports = function scan (queryPath, excludePaths = []) {
+  const data = {
+    entry: queryPath,
+    files: [],
+  };
+  const excludedPaths = [];
+  const loadedExclude = [];
+
+  let fileCount = 0;
+
+  excludedPaths.push.apply(excludedPaths, excludePaths);
+
+  if (fs.existsSync(excludePath)) {
+    const excludeData = fs.readFileSync(excludePath).toString();
+    loadedExclude.push.apply(excludedPaths, excludeData.split('\n'));
+    excludedPaths.push.apply(excludedPaths, loadedExclude);
+  }
+
+  return walk(queryPath)
+  .then(paths => {
+    paths = paths.filter(filePath => excludedPaths.indexOf(filePath) === -1);
+    console.log(chalk.green(`Processing ${paths.length} file${paths.length === 1 ? '' : 's'}`));
+    if (excludedPaths.length) {
+      console.log(chalk.blue(`Excluding ${excludedPaths.length}  file${paths.length === 1 ? '' : 's'}`));
+    }
+
+    paths.forEach(filePath => {
+      fileCount++;
+      // if (fileCount > 100) {
+      //   return;
+      // }
+
+      const options = {
+        plugins: [],
+        track: false,
+        log: false,
+        resolvePaths: true,
+      };
+
+      const ext = path.extname(filePath).substr(1);
+
+      try {
+        if (ext === 'ts' || ext === 'tsx') {
+          options.plugins.push('typescript');
+        } else if (ext === 'js' || ext === 'jsx') {
+          options.plugins.push('flow');
+        }
+        if (ext.substr(-1) === 'x') {
+          options.plugins.push('jsx');
+        }
+        options.plugins.push('classProperties', 'objectRestSpread');
+
+        console.log(`[${chalk.bold.green(fileCount.toString().padStart(5, '0'))}] ${chalk.green(filePath)} [${chalk.cyan(options.plugins.join(','))}]`);
+        const sourceFile = new SourceFile(filePath, options);
+        const deps = sourceFile.getDependencies();
+        data.files.push({
+          file: filePath,
+          deps,
+          plugins: options.plugins,
+        });
+
+        if (!fs.existsSync(bypassExcludePath)) {
+          fs.appendFileSync(excludePath, filePath + '\n');
+        } else {
+          print(deps);
+        }
+      } catch (e) {
+        console.log(chalk.bold.red(filePath));
+        throw e;
+      }
+    });
+    
+    return data;
+  })
+  .catch(e => {
+    console.log(chalk.red('FAIL: ' + e.stack));
+  });
+};
--- index.js
+++ index.js
@@ -1,94 +1,27 @@
-const path = require('path');
-const SourceFile = require('./astQuery/sourceFile');
-const walk = require('./astQuery/walk');
 const fs = require('fs');
 const chalk = require('chalk');
+const scan = require('./astQuery');
 
 const queryPath = '/Users/achamas/ms/atlaskit-mk-2/packages';
 // const queryPath = '/Users/achamas/ms/atlaskit-mk-2/packages/core/polyfills/string-prototype-includes.js';
 // const queryPath = '/Users/achamas/ms/atlaskit-mk-2/packages/media';
 // const queryPath = './temp/test/exports-cjs-cases.js';
 
-const exclude = [
+const excludePaths = [
   '/Users/achamas/ms/atlaskit-mk-2/packages/bitbucket/codemod-util-shared-styles-to-theme/__fixtures__/CodeFont.js',
   '/Users/achamas/ms/atlaskit-mk-2/packages/bitbucket/codemod-util-shared-styles-to-theme/__fixtures__/Color_simple.js',
   '/Users/achamas/ms/atlaskit-mk-2/packages/bitbucket/codemod-util-shared-styles-to-theme/__fixtures__/GridSize_c.js',
   '/Users/achamas/ms/atlaskit-mk-2/packages/media/media-editor/src/engine/core/binaries/mediaEditor.d.ts',
   '/Users/achamas/ms/atlaskit-mk-2/packages/media/media-editor/src/engine/core/binaries/mediaEditor.js',
 ];
-const loadedExclude = [];
-
-const excludePath = './exclude.txt';
-const bypassExcludePath = './exclude.pause.txt';
-
-if (fs.existsSync(excludePath)) {
-  const exlc = fs.readFileSync(excludePath).toString();
-  loadedExclude.push.apply(exclude, exlc.split('\n'));
-  exclude.push.apply(exclude, loadedExclude);
-}
-
-console.log('excluding ' + exclude.length);
-
-const print = val => console.log(JSON.stringify(val, null, 4));
 
 console.clear();
 
-const json = [];
-let fileCount = 0;
-
-walk(queryPath)
-  .then(paths => {
-    paths.forEach(filePath => {
-      if (exclude.indexOf(filePath) > -1) {
-        console.log(chalk.magenta('excluded: '+  filePath));
-        return;
-      }
-      fileCount++;
-      // if (fileCount > 100) {
-      //   return;
-      // }
-      const options = {
-        plugins: [],
-        track: false,
-        log: false,
-        resolvePaths: true,
-      };
-      const ext = path.extname(filePath).substr(1);
-      try {
-        if (ext === 'ts' || ext === 'tsx') {
-          options.plugins.push('typescript');
-        } else if (ext === 'js' || ext === 'jsx') {
-          options.plugins.push('flow');
-        }
-        if (ext.substr(-1) === 'x') {
-          options.plugins.push('jsx');
-        }
-        options.plugins.push('classProperties', 'objectRestSpread');
-        // add to exclude...
-        console.log(`[${chalk.bold.green(fileCount.toString().padStart(5, '0'))}] ${chalk.green(filePath)} [${chalk.cyan(options.plugins.join(','))}]`);
-        const sourceFile = new SourceFile(filePath, options);
-        const deps = sourceFile.getDependencies();
-        json.push({
-          file: filePath,
-          deps,
-          plugins: options.plugins,
-        });
-        if (!fs.existsSync(bypassExcludePath)) {
-          fs.appendFileSync(excludePath, filePath + '\n');
-        } else {
-          print(deps);
-        }
-      } catch (e) {
-        console.log(chalk.bold.red(filePath));
-        console.log(chalk.red(e.stack));
-        throw e;
-      }
-    });
-    if (!fs.existsSync(bypassExcludePath)) {
-      fs.writeFileSync('./walk.json', JSON.stringify(json, null, 4));
-    }
-    console.log(paths.length + ' files parsed!');
+scan(queryPath, excludePaths)
+  .then(data => {
+    fs.writeFileSync('./scan.json', JSON.stringify(data, null, 4));
+    console.log('done.');
   })
   .catch(e => {
-    console.log(chalk.bold.red('FAIL: ' + e.stack));
+    console.log(chalk.red(e.stack));
   });
